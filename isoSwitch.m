2b3b6e11d78117e22592625fb20a5e585b8cd32f
JBKacerovsky <kacerovsky@gmail.com>
2020-01-13 14:35:57 -0500

    Create isoSwitch.m

---
+++ isoSwitch.m
@@ -0,0 +1,10 @@
+function FV=isoSwitch(FV)
+% tiny function to swith the 1st and 2nd column of vertex coordinates (x % y) in a FV struct
+% for some reason x and ycoordinates are swithed when using the isosurface
+% function to create a triangulated mesh from a binary image (bw matrix),
+% with respect to the original image
+%
+% these three lines switch it back
+temp=FV.vertices;
+FV.vertices(:, 1)=temp(:, 2);
+FV.vertices(:, 2)=temp(:, 1);
